0905d8163511aeae94ff8af9dfc477479e16a743
ServioTRC <servio_trc@yahoo.com.mx>
2018-09-17 09:30:20 -0500

    Initial commit

---
+++ app/src/main/java/mx/itesm/strc/busca_libros/Pantalla_Principal.java
@@ -0,0 +1,13 @@
+package mx.itesm.strc.busca_libros;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Pantalla_Principal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pantalla__principal);
+    }
+}
