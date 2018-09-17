450b6797fcdd0bdad2fd243b407e47bf0a888799
ServioTRC <servio_trc@yahoo.com.mx>
2018-09-17 10:24:58 -0500

    Aumentos

--- app/src/main/java/mx/itesm/strc/busca_libros/Pantalla_Principal.java
+++ app/src/main/java/mx/itesm/strc/busca_libros/Pantalla_Principal.java
@@ -1,13 +1,39 @@
 package mx.itesm.strc.busca_libros;
 
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.SearchView;
 
 public class Pantalla_Principal extends AppCompatActivity {
+
+    private EditText isbn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pantalla__principal);
+        isbn = findViewById(R.id.busqueda);
+    }
+
+    public void iniciarBusqueda(View v){
+        String input = isbn.getText().toString();
+        if(Lectura_Web.validar_existencia(input)){
+            Resultados res = new Resultados();
+            FragmentTransaction transaccion = getSupportFragmentManager().beginTransaction();
+            transaccion.replace(R.id.fragment, res);
+            transaccion.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_CLOSE);
+            transaccion.addToBackStack(null);
+            transaccion.commit();
+        } else{
+            AlertDialog.Builder dialogo = new AlertDialog.Builder(this);
+            dialogo.setTitle("Aviso")
+                    .setMessage("ISBN no encontrado")
+                    .setPositiveButton("Aceptar", null)
+                    .show();
+        }
     }
 }
--- app/src/main/java/mx/itesm/strc/busca_libros/Resultados.java
+++ app/src/main/java/mx/itesm/strc/busca_libros/Resultados.java
@@ -6,13 +6,13 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class Resultados extends Fragment {
-
 
     public Resultados() {
         // Required empty public constructor
@@ -26,4 +26,17 @@ public class Resultados extends Fragment {
         return inflater.inflate(R.layout.fragment_resultados, container, false);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+        View v = getView();
+        if( v != null){
+            TextView titulo = v.findViewById(R.id.titulo);
+            TextView autores = v.findViewById(R.id.autores);
+            TextView descripcion = v.findViewById(R.id.descripcion);
+            titulo.setText(Lectura_Web.titulo);
+            autores.setText(Lectura_Web.autores);
+            descripcion.setText(Lectura_Web.descripcion);
+        }
+    }
 }
